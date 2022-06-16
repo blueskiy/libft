5d0f6cc008147548ff66adc25507aa9710edad7a
fimachad <fimachad@student.42.rio>
2022-06-16 12:31:45 -0300

    agora vai hein

--- ft_split.c
+++ ft_split.c
@@ -6,34 +6,13 @@
 /*   By: fimachad <fimachad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 20:14:35 by fimachad          #+#    #+#             */
-/*   Updated: 2022/06/16 11:07:18 by fimachad         ###   ########.fr       */
+/*   Updated: 2022/06/16 12:30:36 by fimachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*get_chars(const char *s, char c)
-{
-	size_t	k;
-	char	*str;
-
-	k = 0;
-	while ((s[k] != c) && (s[k] != '\0'))
-		k++;
-	str = (char *)malloc((k * sizeof(char)) + 1);
-	if (str == NULL)
-		return (NULL);
-	k = 0;
-	while (s[k] != c && s[k] != '\0')
-	{
-		str[k] = s[k];
-		k++;
-	}
-	str[k] = '\0';
-	return (str);
-}
-
-static size_t	ptr_count(const char *s, char c)
+static size_t	ptr_count(char *s, char c)
 {
 	size_t	i;
 	size_t	j;
@@ -42,7 +21,7 @@ static size_t	ptr_count(const char *s, char c)
 	j = 0;
 	while (s[j])
 	{
-		if (s[j] != c && (j == 0))
+		if ((s[j] != c) && (j == 0))
 			i++;
 		else if ((s[j] != c) && ((s[j - 1] == c)))
 			i++;
@@ -51,29 +30,47 @@ static size_t	ptr_count(const char *s, char c)
 	return (i);
 }
 
+static char	*get_chars(char *s, char c)
+{
+	size_t	k;
+	char	*str;
+
+	k = 0;
+	while ((s[k] != c) && (s[k] != '\0'))
+		k++;
+	str = (char *)malloc((k) + 1);
+	if (str == NULL)
+		return (NULL);
+	k = 0;
+	while ((s[k] != c) && (s[k] != '\0'))
+	{
+		str[k] = s[k];
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
+}
+
 char	**ft_split(const char *s, char c)
 {
 	size_t	k;
 	size_t	z;
+	char	*str;
 	char	**chars;
 
 	if (!s)
 		return (NULL);
-	k = 0;
-	while (s[k] == c || s[k] == '\0')
-		k++;
-	if (s[k] == '\0')
-		return (NULL);
+	str = (char *)s;
 	k = 0;
 	z = 0;
-	chars = (char **)malloc((ptr_count(s, c) * sizeof(char *)) + 1);
+	chars = malloc((ptr_count(str, c) + 1) * sizeof(char *));
 	if (chars == NULL)
 		return (NULL);
-	while (z < ptr_count(s, c))
+	while (z < ptr_count(str, c))
 	{
-		while ((s[k] == c) && s[k])
+		while ((str[k] == c) && str[k])
 			k++;
-		chars[z] = get_chars(&s[k], c);
+		chars[z] = get_chars(&str[k], c);
 		k = k + ft_strlen(chars[z]);
 		z++;
 	}
